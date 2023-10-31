007e701e043aff03b245c2859f00d552f75c4c1c
Pablo <pablobentivengo@gmail.com>
2023-10-31 18:57:57 -0300

    testin new deploy

--- src/telegram/index.ts
+++ src/telegram/index.ts
@@ -4,21 +4,19 @@ import server from "../app";
 const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-const port = process.env.PORT || 3000;
+const port = parseInt(process.env.PORT || "3000");
 const domain = process.env.DEPLOY_URL;
 const path = process.env.DEPLOY_PATH;
+
+const webhook = async () => await bot.createWebhook({ domain });
+
+server.post(`/telegraf/${bot.secretPathComponent()}`, async () => webhook());
 
 bot.command("dolar", async () => {
   const rate = await getDollarRate();
   Telegraf.reply(rate);
 });
 
-console.log(port, path, domain);
-
-bot.launch({
-  webhook: {
-    domain: domain,
-    port: port,
-    path: path,
-  },
-});
+server
+  .listen({ port: port })
+  .then(() => console.log("Listening on port", port));
