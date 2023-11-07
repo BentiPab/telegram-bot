f647713c6c3800d151279d62e8f262bc8a4d244c
Pablo <pablobentivengo@gmail.com>
2023-11-07 09:24:51 -0300

    require modules

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ require("./env/index");
 require("./mongo/index");
 require("./telegram/index");
 require("./services/index");
-require("./api/index");
+require("./app/index");
 require("./logger/index");
