bb599162daf1b996311615825a6157a5339fd067
Pablo <pablobentivengo@gmail.com>
2023-11-07 08:36:08 -0300

    testing bot webhook

--- api/index.ts
+++ api/index.ts
@@ -9,15 +9,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bot.webhookCallback(`/api/${process.env.WEBHOOK_PATH!}`));
 app.get("/api", (req, res) => {
-  res.send(process.env.WEBHOOK_PATH);
+  res.send(bot.telegram.getWebhookInfo());
   res.send("Working!!");
 });
 
 app.post(`/api/${process.env.WEBHOOK_PATH!}`, (req, res) => {
   bot.handleUpdate(req.body, res);
 });
-
-logger.log("info", bot.telegram.getWebhookInfo());
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Running on port ${process.env.PORT || 3000}`);
